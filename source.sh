71a0c29c41fac96127239a186908a37f9e6e9300
jk983294 <jk983294@gmail.com>
2020-06-02 18:20:47 +0800

    data

---
+++ source.sh
@@ -0,0 +1,2 @@
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00236/seeds_dataset.txt
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls
